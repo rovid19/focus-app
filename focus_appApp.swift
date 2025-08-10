c05bbe89d315cd2d5248e59e4cf3e553014847ae
Roberto <118137968+rovid19@users.noreply.github.com>
2025-08-10 11:20:50 +0200

    Initial commit

---
+++ focus_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  focus_appApp.swift
+//  focus-app
+//
+//  Created by Roberto Vidovic on 10.08.2025..
+//
+
+import SwiftUI
+
+@main
+struct focus_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
